b103f2d32eb034bcf0be600d6e36f33d26362bf1
Kelven-aguiar <kelvendiv@hotmail.com>
2024-06-11 18:39:39 -0300

    Esse aqui juj

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState } from "react";
 import {
 	Dialog,
 	DialogContent,
@@ -8,14 +8,23 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "./dialog";
-import { useState } from "react";
-
-const [name, setName] = useState();
-function registerClient() {}
-
-const handleRegister = () => {};
 
 export default function Button() {
+	const [clientName, setClientName] = useState("");
+	const [time, setTime] = useState("");
+	const [day, setDay] = useState("");
+	const [petName, setPetName] = useState("");
+	const [reason, setReason] = useState("");
+
+	const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log("Nome do cliente:" + clientName);
+		console.log("Horário: " + time);
+		console.log("Dia: " + day);
+		console.log("Nome do Pet: " + petName);
+		console.log("Motivo:" + reason);
+	};
+
 	return (
 		<Dialog>
 			<DialogTrigger className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
@@ -31,7 +40,13 @@ export default function Button() {
 									Nome do Cliente
 								</label>
 								<input
+									id="clientName"
 									type="text"
+									name="clientName"
+									placeholder="Digite o nome do cliente"
+									onChange={(e) =>
+										setClientName(e.target.value)
+									}
 									className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 								/>
 							</div>
@@ -40,7 +55,10 @@ export default function Button() {
 									Horário
 								</label>
 								<input
+									id="time"
 									type="time"
+									value={time}
+									onChange={(e) => setTime(e.target.value)}
 									className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 								/>
 							</div>
@@ -49,7 +67,10 @@ export default function Button() {
 									Dia
 								</label>
 								<input
+									id="day"
 									type="date"
+									value={day}
+									onChange={(e) => setDay(e.target.value)}
 									className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 								/>
 							</div>
@@ -58,7 +79,10 @@ export default function Button() {
 									Nome do Pet
 								</label>
 								<input
+									id="petName"
 									type="text"
+									value={petName}
+									onChange={(e) => setPetName(e.target.value)}
 									className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 								/>
 							</div>
@@ -66,7 +90,13 @@ export default function Button() {
 								<label className="block text-sm font-medium text-gray-700">
 									Motivo
 								</label>
-								<textarea className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></textarea>
+								<input
+									id="reason"
+									type="text"
+									value={reason}
+									onChange={(e) => setReason(e.target.value)}
+									className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+								/>
 							</div>
 						</form>
 						<button
